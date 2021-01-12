ac40bf2ad06929c429fa1b630f4f7f12b35ac69b
shivam shankhdhar <s.shankhdhar1981@gmail.com>
2021-01-12 14:52:02 +0530

    friend views added

--- friend/views.py
+++ friend/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from accounts.models import Account
+
+def send_Friend_request(request, *args, **kwargs):
+	user = request.user
+	payload = {}
+	if request.method == "POST" and user.is_authenticated:
+		user_id = request.POST.get("receiver_user_id")
+		if user_id:
+			receiver = Account.objects.get(pk = user_id)
+			try:
+				#get any friend requests (active and non-active)
+				friend_requests = FriendRequest.objects.filter(sender = user, receiver = receiver)			
+				# find if any of them are active
+				try:
+					for request in friend_requests:
+						if request.is_active:
+							raise Exception("You already sent them a friend request.")
+						# if non is active, create a friend request
+					friend_request = FriendRequest(sender = sender, receiver = receiver)
+					friend_request.save()
+					payload["response"] = "Friend request sent."
+				except Exception as e:
+					payload['response'] = str(e)
+				
+			except FriendRequest.DoesNotExists:
+				friend_request = FriendRequest(sender = sender, receiver = receiver)
+				friend_request.save()
+				payload['response'] = "Great! Friend request sent."
+
+			if payload['response'] = None:
+				payload['response'] = "Oops! Something went wrong."
+		else:
+			payload["response"] = "Oops! Unable to send friend request"
+	else:
+		payload["response"] = "Oops! You must be authenticated to send them friend request."
+	return HttpResponse(json.dumps(payload), content_type = "application/json")
+			
